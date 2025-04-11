e4eb7f85a035bc8f5c3117624043cd525aa7f556
befometh <cris.acevedo9009@gmail.com>
2025-04-11 18:50:40 +0100

    Commit inicial, esquema general del website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavComponent} from './componentes/nav/nav.component';
+import {HeaderComponent} from './componentes/header/header.component';
+import {FooterComponent} from './componentes/footer/footer.component';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, HeaderComponent, FooterComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'La_Leyenda';
+  title = 'La Leyenda del Libro';
+  pagPrincipal: boolean = true;
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {InicioComponent} from './paginas/inicio/inicio.component';
+import {ErrorComponent} from './paginas/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"inicio", component:InicioComponent},
+  {path:"", redirectTo:"inicio", pathMatch:"full"},
+
+  {path:"**", component:ErrorComponent}
+];
