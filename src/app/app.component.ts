3e600ba6e19e48f0f05f4fd85dc27ac061c04e31
befometh <cris.acevedo9009@gmail.com>
2025-04-11 20:57:20 +0100

    Finalización del header y la tarjeta de usuario del header, posicionamiento de elementos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import {NavComponent} from './componentes/nav/nav.component';
 import {HeaderComponent} from './componentes/header/header.component';
 import {FooterComponent} from './componentes/footer/footer.component';
-import {NgIf} from '@angular/common';
+import {NgClass, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent, HeaderComponent, FooterComponent, NgIf],
+  imports: [RouterOutlet, NavComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'La Leyenda del Libro';
-  pagPrincipal: boolean = true;
+  pagPrincipal: boolean = false;
 }
