04fac9346da47a926c22c45b937a16477cf5e4ea
befometh <cris.acevedo9009@gmail.com>
2025-04-14 21:00:40 +0100

    Posicionamiento del slider, se evita redimensionamiento y se posicionan los botones de desplazamiento, pendiente funcionamiento y ocultado de las imágenes fuera del slider

--- src/app/paginas/catalogo/catalogo.component.ts
+++ src/app/paginas/catalogo/catalogo.component.ts
@@ -11,4 +11,13 @@ export class CatalogoComponent {
   filtrar: string = "filtrar.svg";
   ordenar: string = "ordenar.svg";
 
+  img1="catalogo 1.jpg";
+  img2="catalogo 2.jpg";
+  img3="catalogo 3.jpg";
+  img4="catalogo 4.jpg";
+  img5="catalogo 5.jpg";
+  img6="catalogo 6.jpg";
+  img7="catalogo 7.jpg";
+  img8="catalogo 8.jpg";
+
 }
